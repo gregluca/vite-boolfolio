9dab1d9c7c474c8a9557f3500e82930d663830bf
Luca Gregori <luca.gregori98@gmail.com>
2024-02-03 18:15:41 +0100

    add. axios call

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script>
+import AppMain from './components/AppMain.vue'; 
+
 export default{
+
+  components: {AppMain},
+
   data(){
     return{
 
     }
+    
   }
 }
 </script>
 
 <template>
    <h1>ciao</h1>
+   <AppMain/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios';
+
+export default{
+  data(){
+    return{
+        baseUrl: 'http://127.0.0.1:8000',
+        projects: []
+
+    }
+  },
+  created() {
+    axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
+        console.log(resp);
+        this.projects = resp.data.results;
+    })
+  }
+}
+
+</script>
+
+<template>
+   <h1>main</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
